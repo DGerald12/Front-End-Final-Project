0992a164ae1b009a90cea951ee39558eff1eb11e
Donn Gerald <godsmathetes@gmail.com>
2019-12-20 20:03:45 -0500

    Personal Website

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $("Icons").hover(function(){
+        $(this).css("background-color", "black");
+        }, function(){
+        $(this).css("background-color", "pink");
+    });
+});
